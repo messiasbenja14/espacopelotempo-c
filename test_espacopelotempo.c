c9b3d7ccb6a3cab97b5e5dcdb2b9af4d7efe3068
Messias <messias.benja14@gmail.com>
2026-07-28 22:36:50 -0300

    Update test_espacopelotempo.c

--- test_espacopelotempo.c
+++ test_espacopelotempo.c
@@ -1,11 +1,11 @@
-#include "function_espacopelotempo.c"
 #include <stdio.h>
 #include <assert.h>
+#include "function_espacopelotempo.c"
 
 int main(int argc, char const *argv[])
 {
 	assert(espacopelotempo(80, 2, 0) == 160 && espacopelotempo(40, 2, 0) == 80 && espacopelotempo(40, 2, 3) == 86);
-	printf("Teste concluido com sucesso!");
+	printf("Teste concluido com sucesso!\n");
 
 	return 0;
 }
